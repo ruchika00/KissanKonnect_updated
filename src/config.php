18a66b13e7d8eeb5adba1ff8895b9b57f07d9f6b
SaritaPaliwal <saritapaliwal566@gmail.com>
2025-12-24 21:32:58 +0530

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,8 +1,9 @@
 <?php
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'kissankonnect';
-$db_host = 'localhost';
+$db_username = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+$db_name     = getenv('DB_NAME');
+$db_host     = getenv('DB_HOST');
+
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
