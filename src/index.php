81b73d7544a164dd8f0305d961006a3de67ec65d
sarita <saritapaliwal566@gmail.com>
2025-12-25 21:31:55 +0530

    in one folder

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
     <h1 style="color:green"><b>Kissan Sathi</b></h1>
     <h3 style="color:grey"><i>An online platform where farmer can sell their vegetables via internet</i></h3>
     <br>
-    <img src="./images/pic1.jpeg" height="10" />
+    <img src="images/pic1.jpeg" height="10" />
 </div>
 
 <?php
